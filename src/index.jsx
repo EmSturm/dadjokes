51264fd73e94b771b6071d5bcbfc8759bd55a28b
EmSturm <emsturmova@gmail.com>
2023-11-25 17:31:50 +0100

    Bod č. 15 - Vtip v Reactu

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import { HomePage } from "./pages/HomePage";
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.querySelector('#app')).render(
+    <HomePage />,
+  );
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./style.css";
+
+export const HomePage = () => {
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
+
+  return (
+    <div className="container">
+      <div className="container">
+        <div className="joke">
+          <div className="joke__body">
+            <div className="joke__user">
+              <img className="user-avatar" src="../img/user01.png" />
+              <p className="user-name">Neroxx</p>
+            </div>
+
+            <p className="joke__text">
+              The secret service isn't allowed to yell "Get down!" anymore when
+              the president is about to be attacked. Now they have to yell
+              "Donald, duck!"
+            </p>
+          </div>
+          <div className="joke__likes">
+            <button
+              onClick={() => {
+                setLike(like + 1);
+              }}
+              id="btn-up"
+              className="btn-like btn-like--up"
+            ></button>
+            <span id="likes-up" className="likes-count likes-count--up">
+              {like}
+            </span>
+            <button
+              onClick={() => {
+                setDislike(dislike + 1);
+              }}
+              id="btn-down"
+              className="btn-like btn-like--down"
+            ></button>
+            <span id="likes-down" className="likes-count likes-count--down">
+              {dislike}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
